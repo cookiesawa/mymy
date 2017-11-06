d3fed2803c7b44f5067ef620bc435b86b13898c9
cookiesawa <cookiesawa@naver.com>
2017-11-06 23:47:00 +0900

    g

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]','127.0.0.1', '.pythonanywhere
 # Application definition
 
 INSTALLED_APPS = [
+    'stu',
     'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
